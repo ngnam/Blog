d49e5aec0128c1c9f6867a2000537b3789db6d07
ngnam <nguyenvannam0411@gmail.com>
2021-08-16 02:14:29 +0700

    add blog

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -31,11 +31,8 @@ const AppWrapper = styled.div`
 export default function App() {
   return (
     <AppWrapper>
-      <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
-      >
-        <meta name="description" content="A React.js Boilerplate application" />
+      <Helmet titleTemplate="%s - My Blogs" defaultTitle="My·Blogs">
+        <meta name="description" content="My Blogs" />
       </Helmet>
       <Header />
       <Switch>
